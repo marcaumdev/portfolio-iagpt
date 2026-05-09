809948d3f27b6d158cd27df0f05c17ff15e6ff57
Marcos Vinicius Oliveira Dos Santos <marcos.dossantos@sp.senai.br>
2026-05-09 17:28:46 -0300

    primeira versao do portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+const botaoMensagem = document.getElementById("botaoMensagem")
+const mensagem = document.getElementById("mensagem")
+
+function alterarTexto(){
+    // mensagem.textContent = "Bem-vindo ao meu portfólio! Este projeto foi criado com HTML, CSS e JavaScript."
+    alert('Bem-vindo ao meu portfólio! Este projeto foi criado com HTML, CSS e JavaScript.')
+}
